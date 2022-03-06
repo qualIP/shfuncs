48961dba88b2aa8a320fb14a8d764d3f5a70cb8b
Jean-Sebastien Trottier <jst@qualipsoft.com>
2022-03-06 01:06:51 -0500

    func-git: Add git_path, git_state

--- func-git.sh
+++ func-git.sh
@@ -29,6 +29,33 @@ typeset -f test_git_changeset > /dev/null && return
 # shellcheck disable=SC2034
 EGIT_BISECT_CANT_TEST=125
 
+# See https://github.com/libgit2/libgit2/blob/1327dbcf2a4273a8ba6fd978db5f0882530af94d/src/libgit2/refs.h
+GIT_REFS_DIR="refs/"
+GIT_REFS_HEADS_DIR="${GIT_REFS_DIR}heads/"
+GIT_REFS_TAGS_DIR="${GIT_REFS_DIR}tags/"
+GIT_REFS_REMOTES_DIR="${GIT_REFS_DIR}remotes/"
+GIT_REFS_NOTES_DIR="${GIT_REFS_DIR}notes/"
+GIT_RENAMED_REF_FILE="${GIT_REFS_DIR}RENAMED-REF"
+GIT_PACKEDREFS_FILE="packed-refs"
+GIT_HEAD_FILE="HEAD"
+GIT_ORIG_HEAD_FILE="ORIG_HEAD"
+GIT_FETCH_HEAD_FILE="FETCH_HEAD"
+GIT_MERGE_HEAD_FILE="MERGE_HEAD"
+GIT_REVERT_HEAD_FILE="REVERT_HEAD"
+GIT_CHERRYPICK_HEAD_FILE="CHERRY_PICK_HEAD"
+GIT_BISECT_LOG_FILE="BISECT_LOG"
+GIT_REBASE_MERGE_DIR="rebase-merge/"
+GIT_REBASE_MERGE_INTERACTIVE_FILE="${GIT_REBASE_MERGE_DIR}interactive"
+GIT_REBASE_APPLY_DIR="rebase-apply/"
+GIT_REBASE_APPLY_REBASING_FILE="${GIT_REBASE_APPLY_DIR}rebasing"
+GIT_REBASE_APPLY_APPLYING_FILE="${GIT_REBASE_APPLY_DIR}applying"
+GIT_SEQUENCER_DIR="sequencer/"
+GIT_SEQUENCER_HEAD_FILE="${GIT_SEQUENCER_DIR}head"
+GIT_SEQUENCER_OPTIONS_FILE="${GIT_SEQUENCER_DIR}options"
+GIT_SEQUENCER_TODO_FILE="${GIT_SEQUENCER_DIR}todo"
+GIT_STASH_FILE="stash"
+GIT_REFS_STASH_FILE="${GIT_REFS_DIR}$GIT_STASH_FILE"
+
 ## test_git_changeset [untracked_ok?]
 test_git_changeset() {
     if [[ -z "${OUT_TMP:-}" ]] ; then
@@ -74,13 +101,57 @@ test_git_changeset() {
 }
 
 ## git_dir
+#
+# Returns the relative location of the .git directory.
+# Defaults to GIT_DIR environment variable.
 git_dir() {
     git rev-parse --git-dir
+}
+
+## git_path path
+#
+# Resolves "$GIT_DIR/<path>".
+git_path() {
+    git rev-parse --git-path "$1"
 }
 
 ## git_toplevel
 git_toplevel() {
     git rev-parse --show-toplevel
+}
+
+## git_state
+git_state() {
+    # See https://github.com/libgit2/libgit2/blob/main/src/libgit2/repository.c
+    # Much faster to use $git_dir than rely on git_path for every file.
+    local git_dir=$(git_dir)
+    if [[ -f "$git_dir/$GIT_REBASE_MERGE_INTERACTIVE_FILE" ]] ; then
+        echo "rebase_interactive"
+    elif [[ -d "$git_dir/$GIT_REBASE_MERGE_DIR" ]] ; then
+        echo "rebase_merge"
+    elif [[ -f "$git_dir/$GIT_REBASE_APPLY_REBASING_FILE" ]] ; then
+        echo "rebase"
+    elif [[ -f "$git_dir/$GIT_REBASE_APPLY_APPLYING_FILE" ]] ; then
+        echo "apply_mailbox"
+    elif [[ -d "$git_dir/$GIT_REBASE_APPLY_DIR" ]] ; then
+        echo "apply_mailbox_or_rebase"
+    elif [[ -f "$git_dir/$GIT_MERGE_HEAD_FILE" ]] ; then
+        echo "merge"
+    elif [[ -f "$git_dir/$GIT_REVERT_HEAD_FILE" ]] ; then
+        if [[ -f "$git_dir/$GIT_SEQUENCER_TODO_FILE" ]] ; then
+            echo "revert_sequence"
+        else
+            echo "revert"
+        fi
+    elif [[ -f "$git_dir/$GIT_CHERRYPICK_HEAD_FILE" ]] ; then
+        if [[ -f "$git_dir/$GIT_SEQUENCER_TODO_FILE" ]] ; then
+            echo "cherrypick_sequence"
+        else
+            echo "cherrypick"
+        fi
+    elif [[ -f "$git_dir/$GIT_BISECT_LOG_FILE" ]] ; then
+        echo "bisect"
+    fi
 }
 
 # vim: ft=bash
